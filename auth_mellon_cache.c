12ddffaec5f46617bfa3b73f552bca746cb51e90
Jarek Polok <jpolok@xenophile.cern.ch>
2014-11-13 09:58:25 +0100

    fixes for multi-valued variables:
    - use apr_pstrcat() just once on variable prefix.
    - removed not-needed temp. var for current value.
    - fixed logic: apr_strnatcmp() removed as not needed.

--- auth_mellon_cache.c
+++ auth_mellon_cache.c
@@ -521,7 +521,7 @@ void am_cache_env_populate(request_rec *r, am_cache_entry_t *t)
     const char *varname;
     const char *varname_prefix;
     const char *value;
-    const char *curr_value;
+    const char *prefixed_varname;
     int *count;
     int status;
 
@@ -582,6 +582,8 @@ void am_cache_env_populate(request_rec *r, am_cache_entry_t *t)
             }
         }
 
+        prefixed_varname = apr_pstrcat(r->pool, varname_prefix, varname, NULL);
+
         /* Find the number of times this variable has been set. */
         count = apr_hash_get(counters, varname, APR_HASH_KEY_STRING);
         if(count == NULL) {
@@ -592,39 +594,32 @@ void am_cache_env_populate(request_rec *r, am_cache_entry_t *t)
             apr_hash_set(counters, varname, APR_HASH_KEY_STRING, count);
 
             /* Add the variable without a suffix. */
-            apr_table_set(r->subprocess_env,
-                          apr_pstrcat(r->pool, varname_prefix, varname, NULL),
-                          value);
+            apr_table_set(r->subprocess_env,prefixed_varname,value);
         }
 
+        if (d->merge_env_vars != 1) {
+         
+            /* Add the variable with a suffix indicating how many times it has
+             * been added before.
+             */
+            apr_table_set(r->subprocess_env,
+                          apr_psprintf(r->pool, "%s_%d", prefixed_varname, *count),
+                          value);
 
-        if (d->merge_env_vars == 1) {
+        } else if (*count > 0) {
 
             /*
              * Merge multiple values, separating with ";" 
              * this makes auth_mellon work same way mod_shib is:
              * https://wiki.shibboleth.net/confluence/display/SHIB2/NativeSPAttributeAccess
              */
-            curr_value=(char *)apr_table_get(r->subprocess_env,
-                                             apr_pstrcat(r->pool, varname_prefix, varname, NULL)); 
-            /* avoid duplicated values - from parent req. ? */
-            if (apr_strnatcmp(curr_value,value)!= 0) {
-                apr_table_set(r->subprocess_env,
-                              apr_pstrcat(r->pool, varname_prefix, varname, NULL),
-                              apr_pstrcat(r->pool, value, ";" , curr_value, NULL));
-            }
-
-        } else {
-
-            /* Add the variable with a suffix indicating how many times it has
-             * been added before.
-             */
-            apr_table_set(r->subprocess_env,
-                          apr_psprintf(r->pool, "%s%s_%d", varname_prefix, varname, *count),
-                          value);
-
+             apr_table_set(r->subprocess_env,
+                           prefixed_varname,
+                           apr_pstrcat(r->pool, 
+                                       apr_table_get(r->subprocess_env,prefixed_varname),
+                                       ";", value, NULL));
         }
-
+          
         /* Increase the count. */
         ++(*count);
     }
